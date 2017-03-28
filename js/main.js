8ce112245623ca486cc6e51710b664590f8f6462
Kachitaa Avila <Kachitaa Avila>
2017-03-27 21:41:04 -0600

    New content

--- js/main.js
+++ js/main.js
@@ -1,22 +1,58 @@
-'use strict';
+$(document).ready(init);
 
-var arrowDown    = $('#arrow-down');
-var navbar       = $('.navbar-default');
-var navHome      = $('.navHome');
-var navAbout     = $('.navAbout');
-var navResume    = $('.navResume');
-var navPortfolio = $('.navPortfolio');
-var navContact   = $('.navContact');
-var navBlog      = $('.navBlog'); 
+function init(){
+   $("#btn-1").mouseover(function(){
+	 $(this).removeClass("mdi mdi-lightbulb-outline");
+	 $(this).addClass("mdi mdi-lightbulb-on-outline");  	
+   });
 
-$(document).on('ready', function(){
-	var scroll_pos = 0;
-	$(document).scroll(function(){
-		scroll_pos = $(this).scrollTop();
-		if(scroll_pos > 800){
-			navbar.addClass('navbar-default');
-		}
-		else {
-			navbar.removeClass('navbar-transform');
-		}
+   $("#btn-1").mouseout(function(){
+   	$(this).removeClass("mdi mdi-lightbulb-on-outline");
+	 $(this).addClass("mdi mdi-lightbulb-outline");
+	   	
+   });
+};
+
+/*Don't repeat "function init() for the same function"*/
+
+$("#btn-2").mouseover(function(){
+	 $(this).removeClass("mdi mdi-arrow-right-drop-circle-outline");
+	 $(this).addClass("mdi mdi-arrow-down-drop-circle");  	
+   });
+
+   $("#btn-2").mouseout(function(){
+   	$(this).removeClass("mdi mdi-arrow-down-drop-circle");
+	 $(this).addClass("mdi mdi-arrow-right-drop-circle-outline"); 	
+   });
+
+
+$("#btn-3").mouseover(function(){
+	 $(this).removeClass("mdi mdi-lightbulb-outline");
+	 $(this).addClass("mdi mdi-lightbulb-on-outline");  	
+   });
+
+   $("#btn-3").mouseout(function(){
+   	$(this).removeClass("mdi mdi-lightbulb-on-outline");
+	 $(this).addClass("mdi mdi-lightbulb-outline"); 	
+   });
+
+$("#btn-4").mouseover(function(){
+	 $(this).removeClass("mdi mdi-arrow-right-drop-circle-outline");
+	 $(this).addClass("mdi mdi-arrow-down-drop-circle");  	
+   });
+
+   $("#btn-4").mouseout(function(){
+   	$(this).removeClass("mdi mdi-arrow-down-drop-circle");
+	 $(this).addClass("mdi mdi-arrow-right-drop-circle-outline"); 	
+   });
+
+$("#btn-2").on('click', function() {
+		$('body').animate({ scrollTop: 800}, 'slow');
 	});
+
+$("#btn-4").on('click', function() {
+		$('body').animate({ scrollTop: 800}, 'slow');
+	});
+
+//!var firstname = "Karen";
+//!alert(firstname +" is awsome!");
